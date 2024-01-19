1e905e5d34b2c902a0f425c3f45648824bc7bd0c
bParz156 <basia15p@gmail.com>
2024-01-19 17:00:51 +0100

    Załadowanie plików

---
+++ camera/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.Drawing;
+
+namespace camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.trackBarZoom = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarHue = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBarSat = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(650, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(428, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(63, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(542, 462);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(714, 124);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(121, 65);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Połącz z kamerą";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.onButtonStartClick);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(910, 124);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(121, 65);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Zapisz zdjęcie";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.onButtonSaveClick);
+            // 
+            // trackBarZoom
+            // 
+            this.trackBarZoom.Location = new System.Drawing.Point(714, 300);
+            this.trackBarZoom.Maximum = 5;
+            this.trackBarZoom.Minimum = 1;
+            this.trackBarZoom.Name = "trackBarZoom";
+            this.trackBarZoom.Size = new System.Drawing.Size(104, 45);
+            this.trackBarZoom.TabIndex = 5;
+            this.trackBarZoom.Value = 1;
+            this.trackBarZoom.Scroll += new System.EventHandler(this.trackBarZoom_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(714, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Zbliżenie (zoom)";
+            // 
+            // trackBarHue
+            // 
+            this.trackBarHue.Location = new System.Drawing.Point(817, 300);
+            this.trackBarHue.Maximum = 360;
+            this.trackBarHue.Name = "trackBarHue";
+            this.trackBarHue.Size = new System.Drawing.Size(104, 45);
+            this.trackBarHue.TabIndex = 5;
+            this.trackBarHue.Value = 180;
+            this.trackBarHue.Scroll += new System.EventHandler(this.trackBarHue_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(835, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Odcień (hue)";
+            // 
+            // trackBarSat
+            // 
+            this.trackBarSat.Location = new System.Drawing.Point(927, 300);
+            this.trackBarSat.Maximum = 100;
+            this.trackBarSat.Name = "trackBarSat";
+            this.trackBarSat.Size = new System.Drawing.Size(104, 45);
+            this.trackBarSat.TabIndex = 5;
+            this.trackBarSat.Value = 50;
+            this.trackBarSat.Scroll += new System.EventHandler(this.trackBarSat_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(936, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nasycenie (saturation)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 572);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBarSat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarHue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarZoom);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Wybierz urządzenie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarHue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TrackBar trackBarZoom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarHue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBarSat;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ camera/Form1.cs
@@ -0,0 +1,124 @@
+﻿using AForge.Imaging.Filters ;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Vision.Motion;
+
+namespace camera
+{
+    public partial class Form1 : Form
+    {
+        Bitmap frame;
+        private Bitmap previousFrame = null;
+        VideoCaptureDevice capturedDevice;
+        HueModifier hueFilter = new HueModifier(180);
+        SaturationCorrection satFilter = new SaturationCorrection(0.5f);
+        FilterInfoCollection videoDeviceList;
+        long zoom = 1;
+        bool isConnected = false;
+        MotionDetector motionDetector;
+        public Form1()
+        {
+            InitializeComponent();
+            videoDeviceList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo videoDevice in videoDeviceList)
+            {
+                comboBox1.Items.Add(videoDevice.Name);
+            }
+            capturedDevice = new VideoCaptureDevice();
+            comboBox1.SelectedIndex = 0;
+            motionDetector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionAreaHighlighting());
+            Task.Run(() => motionDetected());
+        }
+
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            frame = (Bitmap)eventArgs.Frame.Clone();
+            frame = new Bitmap(frame, new Size((int)(frame.Width * zoom), (int)(frame.Height * zoom)));
+            frame = hueFilter.Apply(frame);
+            frame = satFilter.Apply(frame);
+            previousFrame = (Bitmap)frame.Clone();
+            pictureBox1.Image = frame;
+            
+        }
+
+        private void onButtonStartClick(object sender, EventArgs e)
+        {
+            if(!isConnected)
+            {
+                capturedDevice = new VideoCaptureDevice(videoDeviceList[comboBox1.SelectedIndex].MonikerString);
+                capturedDevice.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                capturedDevice.Start();
+                buttonStart.Text = "Rozłącz z kamerą";
+            }
+            else
+            {
+                capturedDevice.SignalToStop();
+                buttonStart.Text = "Połącz z kamerą";
+                pictureBox1.Image = null;
+            }
+            comboBox1.Visible = isConnected;
+            isConnected = !isConnected;
+            pictureBox1.Visible = isConnected;
+        }
+
+        private void onButtonSaveClick(object sender, EventArgs e)
+        {
+            if(isConnected)
+            {
+                SaveFileDialog sfdImage = new SaveFileDialog(); // obiekt pozwalający na wybór lokalizacji i nazwy pliku do zapisania
+                sfdImage.Filter = "(*.jpg)|*.jpg";
+                Image imageToSave= (Image) pictureBox1.Image.Clone();
+                if (sfdImage.ShowDialog() == DialogResult.OK)
+                {
+                    imageToSave.Save(sfdImage.FileName, System.Drawing.Imaging.ImageFormat.Bmp); // zapisanie obrazu do wybranego pliku
+                    MessageBox.Show("Zapisano");
+                }
+                else 
+                    MessageBox.Show("Zapisywanie nie powiodło się");
+            }
+            else
+            {
+                MessageBox.Show("Żadne urządzenie nie zostało podłączone, nie można zrobić zdjęcia");
+            }  
+        }
+
+        public void motionDetected()
+        {
+            while (true)
+            {
+                if (isConnected && previousFrame!=null)
+                {
+                   float f = motionDetector.ProcessFrame(previousFrame);
+                    if(f>0)
+                    {
+                        MessageBox.Show("Wykryto ruch!");
+                    }
+
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+
+        private void trackBarZoom_Scroll(object sender, EventArgs e)
+        {
+            zoom = trackBarZoom.Value;
+        }
+
+        private void trackBarHue_Scroll(object sender, EventArgs e)
+        {
+            hueFilter= new HueModifier(trackBarHue.Value);
+        }
+
+        private void trackBarSat_Scroll(object sender, EventArgs e)
+        {
+            satFilter = new SaturationCorrection(trackBarSat.Value/100);
+        }
+
+    }
+}
